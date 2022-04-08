4665375e567f5c5b75a609effccb9b28bbc4dea1
alegallardo@us.es <alegallardo@us.es>
2022-04-08 10:57:51 +0200

    sigue curso

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss'],
+})
+export class MainPageComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  agregar(){
+    console.log("que hay");
+  }
+
+}
